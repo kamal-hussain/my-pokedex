401e7d71570c19159fe449a97dc44e22fe96d253
Kamal Hussain <75313243+kamal-hussain@users.noreply.github.com>
2024-09-21 19:15:00 +0100

    Modal now has working nav buttons

--- src/app/components/pokemon-modal/pokemon-modal.component.ts
+++ src/app/components/pokemon-modal/pokemon-modal.component.ts
@@ -17,6 +17,8 @@ export class PokemonModalComponent implements OnInit, OnDestroy {
   @Input() pokemon: any;
   @Input() pokemonSpecies: any;
   @Output() close = new EventEmitter<void>();
+  @Output() next = new EventEmitter<void>();
+  @Output() previous = new EventEmitter<void>();
 
   constructor(private pokeApi: PokeApiService) {}
 
@@ -34,13 +36,112 @@ export class PokemonModalComponent implements OnInit, OnDestroy {
     this.close.emit();
   }
 
+  nextPokemon() {
+    if (this.pokemon.id < 898) {
+      this.pokeApi
+        .getPokemonDetails((this.pokemon.id + 1).toString())
+        .subscribe((details: any) => {
+          this.pokemon = details;
+          this.getSpecies();
+        });
+    }
+  }
+
+  previousPokemon() {
+    if (this.pokemon.id > 1) {
+      this.pokeApi
+        .getPokemonDetails((this.pokemon.id - 1).toString())
+        .subscribe((details: any) => {
+          this.pokemon = details;
+          this.getSpecies();
+        });
+    }
+  }
   getSpecies() {
-    this.pokeApi
-      .getPokemonSpeciesDetails(this.pokemon.name)
-      .subscribe((details: any) => {
+    const baseName = this.pokemon.name.split('-')[0];
+    this.pokeApi.getPokemonSpeciesDetails(baseName).subscribe(
+      (details: any) => {
         this.pokemonSpecies = details;
-        console.log(this.pokemonSpecies);
-        console.log(this.pokemon);
-      });
+        console.log(this.pokemon.id);
+      },
+      (error) => {
+        console.error('Error fetching species details:', error);
+      }
+    );
+  }
+
+  getSelectedFlavorTextEntries(): any[] {
+    if (!this.pokemonSpecies?.flavor_text_entries) return [];
+
+    const englishEntries = this.pokemonSpecies.flavor_text_entries.filter(
+      (entry: any) => entry.language.name === 'en'
+    );
+
+    const uniqueEntries = this.getUniqueEntries(englishEntries);
+
+    if (uniqueEntries.length > 4) {
+      return [...uniqueEntries.slice(0, 3), ...uniqueEntries.slice(-3)];
+    } else {
+      return uniqueEntries.slice(0, 4);
+    }
+  }
+
+  private getUniqueEntries(entries: any[]): any[] {
+    const uniqueEntries: any[] = [];
+
+    entries.forEach((entry) => {
+      const similarEntry = uniqueEntries.find(
+        (uniqueEntry) =>
+          this.calculateSimilarity(entry.flavor_text, uniqueEntry.flavor_text) >
+          0.32
+      );
+
+      if (!similarEntry) {
+        uniqueEntries.push(entry);
+      } else if (entry.flavor_text.length > similarEntry.flavor_text.length) {
+        const index = uniqueEntries.indexOf(similarEntry);
+        uniqueEntries[index] = entry;
+      }
+    });
+
+    return uniqueEntries;
+  }
+
+  private calculateSimilarity(str1: string, str2: string): number {
+    const longer = str1.length > str2.length ? str1 : str2;
+    const shorter = str1.length > str2.length ? str2 : str1;
+    const longerLength = longer.length;
+
+    if (longerLength === 0) {
+      return 1.0;
+    }
+
+    return (longerLength - this.editDistance(longer, shorter)) / longerLength;
+  }
+
+  private editDistance(s1: string, s2: string): number {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    const costs: number[] = [];
+    for (let i = 0; i <= s1.length; i++) {
+      let lastValue = i;
+      for (let j = 0; j <= s2.length; j++) {
+        if (i === 0) {
+          costs[j] = j;
+        } else if (j > 0) {
+          let newValue = costs[j - 1];
+          if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
+            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+          }
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+      if (i > 0) {
+        costs[s2.length] = lastValue;
+      }
+    }
+    return costs[s2.length];
   }
 }
