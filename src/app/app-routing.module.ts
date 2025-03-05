de8acfd655b5b2f73988e46a9f5fceddeccc15c7
Kamal Hussain <75313243+kamal-hussain@users.noreply.github.com>
2025-03-05 11:12:38 +0000

    router changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeDetailsComponent } from './components/poke-details/poke-details.component';
+import { DexGridComponent } from './components/dex-grid/dex-grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DexGridComponent },
+  { path: 'pokemon/:name', component: PokeDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
